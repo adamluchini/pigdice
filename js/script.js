57d89f592a15279a21e9f4081658ce1a9416d59e
Adam Luchini and Cory Olson <cory@>
2016-06-16 16:42:19 -0700

    final changes

--- js/script.js
+++ js/script.js
@@ -7,9 +7,8 @@ Player.prototype.hold = function(){
   this.total += this.round;
   this.round = 0;
   $('#player' + newGame.turn).text(newGame.players[newGame.turn].total);
-  var players = newGame.players;
-  var winners = newGame.winCondition(players);
-  if(newGame.turn === players.length - 1){
+  var winners = newGame.winCondition(newGame.players);
+  if(newGame.turn === newGame.players.length - 1){
     if(winners.length > 0){
       winners.forEach(function(winner){
         $('#winners').append("<p>" + winner + "</p>");
@@ -19,12 +18,7 @@ Player.prototype.hold = function(){
       //$('#game-board').toggle();
     }
   }
-
-  if(newGame.turn === players.length - 1){
-    newGame.turn = 0;
-  } else {
-    newGame.turn += 1;
-  }
+  (newGame.turn === newGame.players.length - 1) ? newGame.turn = 0 : newGame.turn += 1;
   $('#player-turn p').text(newGame.turn + 1);
 
 }
@@ -38,7 +32,7 @@ function Game(name, dice, players){
 }
 
 Game.prototype.checkDice = function(dice){
-  if(rules === 1){
+  if(this.name === 1){
     //traditional ruleset
     if(dice[0] > 1){
       this.players[this.turn].round += dice[0];
@@ -46,10 +40,31 @@ Game.prototype.checkDice = function(dice){
       this.players[this.turn].round = 0;
       this.players[this.turn].hold();
     }
-  } else if(rules === 2){
-
+    return true;
+  } else if(this.name === 2){
+    if(dice[0] > 1 && dice[1] > 1){
+      this.players[this.turn].round += dice[0] + dice[1];
+    } else if(dice[0] === 1 || dice[1] === 1){
+      this.players[this.turn].round = 0;
+      this.players[this.turn].hold();
+    } else if(dice[0] === dice[1]){
+      this.players[this.turn].round += dice[0] + dice[1];
+    } else {
+      return false;
+    }
   } else if(rules === 3){
-
+    if((dice[0] > 1 && dice[1] > 1) && !(dice[0] === dice[1])){
+      this.players[this.turn].round += dice[0] + dice[1];
+    } else if(dice[0] === 1 && dice[1] === 1){
+      this.players[this.turn].round += 25;
+    } else if(dice[0] === 1 || dice[1] === 1){
+      this.players[this.turn].round = 0;
+      this.players[this.turn].hold();
+    } else if(dice[0] === dice[1]){
+      this.players[this.turn].round += 2 * (dice[0] + dice[1]);
+    } else {
+      return false;
+    }
   } else {
 
   }
@@ -60,18 +75,15 @@ Game.prototype.checkDice = function(dice){
 
 Game.prototype.roller = function(){
   var result = [];
-  if(this.dice === 1){
+  var i = 0
+  while(i < this.dice){
     result.push(Math.floor(Math.random() * (6)) + 1);
-  } else {
-    for(var i = 0; i < this.dice; i++){
-      result.push(Math.floor(Math.random() * (6)) + 1);
-    }
+    i++;
   }
   return result;
 }
 
 Game.prototype.winCondition = function(){
-
   var playerLen = this.players.length;
   var winners = []
   for(var i = 0; i < playerLen; i++){
@@ -107,9 +119,10 @@ $(function(){
       i++
     }
 
-    var gameType = $('select#game-type-sel').val();
+    var gameType = parseInt($('select#game-type-sel').val());
     var dice = 0;
-    (gameType === "Pig") ? dice = 1 : dice = 2;
+
+    (gameType === 1) ? dice = 1 : dice = 2;
 
     newGame = new Game(gameType, dice, players);
 
@@ -127,13 +140,17 @@ $(function(){
   $('#roll-dice-button').click(function(event){
     console.log(JSON.stringify(newGame));
     var rando = newGame.roller();
-
+    console.log(rando);
     $('#die-face').text("");
     rando.forEach(function(num){
       var hex = 9855 + num;
-      $('#die-face').html('&#' + hex + ";");
+      $('#die-face').append('&#' + hex + ";");
     })
-
+    if(rando.length > 1 && rando[0] === rando[1]){
+      $('#hold-button').prop("disabled", true);
+    } else {
+      $('#hold-button').prop("disabled", false);
+    }
     newGame.checkDice(rando);
   });
 
